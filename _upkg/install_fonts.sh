8125bba3bb788dd70bf64d24bc6e10d4707ae87e
Jeff Buttars <jeffbuttars@gmail.com>
2015-12-02 11:02:08 -0700

    Move nerdy font install into this repo

---
+++ _upkg/install_fonts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pr_info "Updating/installing fonts..."
+
+GIT_DST_DIR="${PWD}/nerd-fonts"
+FONT_DST_DIR='~/fonts'
+
+mkdir -p "$FONT_DST_DIR"
+
+if [[ -d "$GIT_DST_DIR" ]]; then
+    cd "$GIT_DST_DIR"
+    git pull
+    cd -
+else
+    git clone https://github.com/ryanoasis/nerd-fonts.git "$GIT_DST_DIR"
+fi
+
+# rsync -rc "$GIT_DST_DIR/glyph-source-fonts/devicons.ttf" "$FONT_DST_DIR/"
+cp -f "$GIT_DST_DIR/glyph-source-fonts/devicons.ttf" "$FONT_DST_DIR/"
+
+cd "$GIT_DST_DIR"
+./install.sh
+cd -
