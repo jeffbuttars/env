d73fcf2347fc99453ecda46f580fbdb9ed181477
Jeff Buttars <jeffbuttars@gmail.com>
2018-06-07 10:18:27 -0600

    Disable npm prefix in preference of nvm

--- src/dotfiles/shell.d/common/node.sh
+++ src/dotfiles/shell.d/common/node.sh
@@ -1,14 +1,7 @@
-# Localize the global nodejs installs
-export NPM_PACKAGES="$HOME/.npm-packages"
+########################################
+# NODE_PATH/NPM_PACKAGES Does not play with NVM
+########################################
 
-export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
-
-if [[ ! -d "$NPM_PACKAGES" ]]; then
-    mkdir -p "$NPM_PACKAGES"
-fi
-
-# MANPATH
-export MANPATH="$(manpath):$NPM_PACKAGES/share/man"
 
 # NVM
 if [[ -f /usr/share/nvm/init-nvm.sh  ]]; then
@@ -19,3 +12,17 @@ mkdir -p $HOME/.nvm
 export NVM_DIR=$HOME/.nvm
 [ -f $NVM_DIR/nvm.sh ] && source $NVM_DIR/nvm.sh
 
+
+if [[ -z $NVM_DIR  ]]; then
+    # Localize the global nodejs installs
+    export NPM_PACKAGES="$HOME/.npm-packages"
+
+    export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+
+    if [[ ! -d "$NPM_PACKAGES" ]]; then
+        mkdir -p "$NPM_PACKAGES"
+    fi
+
+    # MANPATH
+    export MANPATH="$(manpath):$NPM_PACKAGES/share/man"
+fi
