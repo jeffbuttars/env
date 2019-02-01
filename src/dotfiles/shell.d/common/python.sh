10f768b8c8823f8f796ee760baf34858956e2727
Jeff Buttars <jeffbuttars@gmail.com>
2019-02-01 13:35:30 -0700

    Much i3 testing

--- src/dotfiles/shell.d/common/python.sh
+++ src/dotfiles/shell.d/common/python.sh
@@ -1,5 +1,5 @@
 
-export VC_VENV_INITIAL_DEV_PKGS="neovim flake8 pylint isort"
+export VC_VENV_INITIAL_DEV_PKGS="pynvim flake8 isort"
 
 if [[ "$HOME/.pythonrc.py" ]]; then
     export PYTHONSTARTUP="$HOME/.pythonrc.py"
