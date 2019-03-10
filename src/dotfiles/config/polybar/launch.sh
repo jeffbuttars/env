b1251eef753c512004b8064d782effce0cfb5da3
Jeff Buttars <jeffbuttars@gmail.com>
2019-03-10 10:59:16 -0600

    polybar updates for ephi

--- src/dotfiles/config/polybar/launch.sh
+++ src/dotfiles/config/polybar/launch.sh
@@ -8,12 +8,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+echo "Launching polybar for $HNAME"
+
 if [[ "$HNAME" == "lola" ]]; then
     # Launch bar1 and bar2
     polybar --reload right &
     polybar --reload middle &
     polybar --reload left &
+elif [[ "$HNAME" == "ephi" ]]; then
+    polybar --reload ephi-left &
+    polybar --reload ephi-middle &
 else
+    echo "Using polybar config 'onescreen'"
     polybar --reload onescreen &
 fi
 
