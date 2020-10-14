8f67f277611d69f979d18342493e71bbc4ebe5e6
Jeff Buttars <jeffbuttars@gmail.com>
2020-10-14 11:07:58 -0600

    wip

--- src/dotfiles/shell.d/common/python.sh
+++ src/dotfiles/shell.d/common/python.sh
@@ -1,6 +1,6 @@
 
 export VC_VENV_INITIAL_DEV_PKGS="pynvim isort black flake8 flake8-isort \
-    flake8-black flake8-bugbear mypy bandit"
+    flake8-black flake8-bugbear mypy bandit data-science-types"
 
 if [[ "$HOME/.pythonrc.py" ]]; then
     export PYTHONSTARTUP="$HOME/.pythonrc.py"
