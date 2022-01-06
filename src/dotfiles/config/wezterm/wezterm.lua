d2314ac8b248e0824a2868ba4385ff27abe8f444
Jeff Buttars <jeffbuttars@gmail.com>
2022-01-06 16:56:27 -0700

    wip

--- src/dotfiles/config/wezterm/wezterm.lua
+++ src/dotfiles/config/wezterm/wezterm.lua
@@ -1,7 +1,9 @@
 local wezterm = require 'wezterm';
 local TERM_META = os.getenv("TERM_META")
+local HOME = os.getenv("HOME")
 
-c_scheme = "Builtin Solarized Light"
+local c_scheme = "Builtin Solarized Light"
+local background_image = HOME .. "/Pictures/Wallpaper/spikegungs.jpg"
 
 if (TERM_META == "dark")
 then
@@ -15,21 +17,23 @@ return {
   use_fancy_tab_bar = true,
   hide_tab_bar_if_only_one_tab = true,
 
+  text_background_opacity = 1.0,
+
   window_background_opacity = 1.0,
   color_scheme = c_scheme,
 
- window_background_image = "../../Pictures/Wallpaper/spikegungs.jpg",
- window_background_image_hsb = {
-    -- Darken the background image by reducing it to 1/3rd
-    brightness = 0.3,
+  -- window_background_image = background_image,
+  -- window_background_image_hsb = {
+  --   -- Darken the background image by reducing it to 1/3rd
+  --   brightness = 0.01,
 
-    -- You can adjust the hue by scaling its value.
-    -- a multiplier of 1.0 leaves the value unchanged.
-    hue = 1.0,
+  --   -- You can adjust the hue by scaling its value.
+  --   -- a multiplier of 1.0 leaves the value unchanged.
+  --   hue = 1.0,
 
-    -- You can adjust the saturation also.
-    saturation = 1.0,
-  },
+  --   -- You can adjust the saturation also.
+  --   saturation = 1.0,
+  -- },
 
   scrollback_lines = 1000000,
   enable_scroll_bar = true,
