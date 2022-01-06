ae96662888d1265aa1823cb9036ba5a56b430c08
Jeff Buttars <jeffbuttars@gmail.com>
2022-01-06 16:37:51 -0700

    Have wezterm set colorscheme base on TERM_META

--- src/dotfiles/config/wezterm/wezterm.lua
+++ src/dotfiles/config/wezterm/wezterm.lua
@@ -1,4 +1,13 @@
 local wezterm = require 'wezterm';
+local TERM_META = os.getenv("TERM_META")
+
+c_scheme = "Builtin Solarized Light"
+
+if (TERM_META == "dark")
+then
+    c_scheme = "Solarized Dark - Patched"
+end
+
 return {
   font = wezterm.font("FiraCode Nerd Font Mono"),
   font_size=14.0,
@@ -7,7 +16,20 @@ return {
   hide_tab_bar_if_only_one_tab = true,
 
   window_background_opacity = 1.0,
-  color_scheme = "Solarized Dark - Patched",
+  color_scheme = c_scheme,
+
+ window_background_image = "../../Pictures/Wallpaper/spikegungs.jpg",
+ window_background_image_hsb = {
+    -- Darken the background image by reducing it to 1/3rd
+    brightness = 0.3,
+
+    -- You can adjust the hue by scaling its value.
+    -- a multiplier of 1.0 leaves the value unchanged.
+    hue = 1.0,
+
+    -- You can adjust the saturation also.
+    saturation = 1.0,
+  },
 
   scrollback_lines = 1000000,
   enable_scroll_bar = true,
