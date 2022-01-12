34a79e73f9122dbd75c22caaf7175c89f1b52be0
Jeff Buttars <jeffbuttars@gmail.com>
2022-01-12 08:34:33 -0700

    Working on a more modular polybar config

--- src/dotfiles/config/polybar/launch.sh
+++ src/dotfiles/config/polybar/launch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 HNAME=$(hostname)
+CONNECTED_SCREENS=$(xrandr | grep ' connected ' | awk '{print $1}')
 
 # Terminate already running bar instances
 killall -q polybar
@@ -28,6 +29,31 @@ elif [[ "$HNAME" == "lola" ]]; then
 elif [[ "$HNAME" == "ephi" ]]; then
     polybar --reload ephi-left &
     polybar --reload ephi-middle &
+elif [[ "$HNAME" == "chica" ]]; then
+    left_display="DP-4"
+    middle_display="DP-1-3"
+    right_display="eDP-1"
+
+    left_screen=""
+    middle_screen=""
+    right_screen=""
+
+    # See what's attached and launch bars accordingly
+    sa=($CONNECTED_SCREENS)
+    [[ ${sa[*]} =~ "$left_display" ]] && left_screen="true" || left_screen=""
+    [[ ${sa[*]} =~ "$right_display" ]] && right_screen="true" || right_screen=""
+
+    echo "Screens found: $sa"
+
+    if [[ $left_screen ]] &&  [[ $right_screen ]] ; then
+        echo "Loading multi monitor config..."
+        polybar --reload chica-left &
+        polybar --reload chica-middle &
+        polybar --reload chica-right &
+    else
+        echo "Loading single monitor config..."
+        polybar --reload onescreen &
+    fi
 else
     echo "Using polybar config 'onescreen'"
     polybar --reload onescreen &
