b09a6c9b9ceba67b64de0b614a8b41701e3b8f12
Jeff Buttars <jeffbuttars@gmail.com>
2022-01-16 16:03:21 -0700

    Do best to auto decect screens and launch polybar accordingly

--- src/dotfiles/config/polybar/launch.sh
+++ src/dotfiles/config/polybar/launch.sh
@@ -1,69 +1,79 @@
 #!/usr/bin/env bash
 
-HNAME=$(hostname)
-CONNECTED_SCREENS=$(xrandr | grep ' connected ' | awk '{print $1}')
-DIR="$HOME/.config/polybar"
-
 # Terminate already running bar instances
 killall -q polybar
-
-export POLYBAR_WLAN=wlp170s0
-export POLYBAR_LAN=enp0s13f0u3u1
-
-polybar --reload main -c "$DIR"/base/config.ini &
-exit 0
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 echo "Launching polybar for $HNAME"
 
-if [[ "$HNAME" == "rose" ]]; then
-    # Launch bar1 and bar2
-    polybar --reload right &
-    polybar --reload middle &
-    polybar --reload left &
+
+HNAME=$(hostname)
+CONNECTED_SCREENS=$(xrandr | grep ' connected ' | awk '{print $1}')
+CONNECTED_SCREENS_INFO=$(xrandr | grep ' connected ' | awk '{print $3}')
+
+CS=($CONNECTED_SCREENS)
+echo "CONNECTED_SCREENS: ${CS[@]}"
+
+CSI=($CONNECTED_SCREENS_INFO)
+echo "CONNECTED_SCREENS_INFO: ${CSI[@]}"
+
+NUM_SCREENS=${#CS[@]}
+
+PRIMARY_DISPLAY=""
+SECONDARY_DISPLAYS=""
+
+if [[ $NUM_SCREENS -ge 1 ]]; then
+    echo "Multiple displays: ${CS[@]}"
+
+    for i in ${!CS[@]}; do
+        if [[ "${CSI[$i]}" == "primary" ]]; then
+            PRIMARY_DISPLAY="${CS[$i]}"
+        else
+            SECONDARY_DISPLAYS="$SECONDARY_DISPLAYS ${CS[$i]}"
+        fi
+    done
+fi
+
+echo ""
+echo "primary_display: $PRIMARY_DISPLAY"
+echo "secondary_displays: $SECONDARY_DISPLAYS"
+
+DIR="$HOME/.config/polybar"
+
+export POLYBAR_WLAN=
+export POLYBAR_LAN=
+export PRIMARY_MONITOR=
+export SECONDARY_MONITOR=
+
+
+if [[ "$HNAME" == "DVJ-BST-Dev-0020-ButtarsLT" ]]; then
+    # BST Laptop
+    export POLYBAR_WLAN=wlp0s20f3
+    export POLYBAR_LAN=enp0s31f6
 elif [[ "$HNAME" == "dvj-bst-dev-0011-buttars" ]]; then
-    # Launch bar1 and bar2
-    polybar --reload right &
-    polybar --reload middle &
-    polybar --reload left &
-elif [[ "$HNAME" == "lola" ]]; then
-    # Launch bar1 and bar2
-    polybar --reload right &
-    polybar --reload middle &
-    polybar --reload left &
+    # BST Workstation
+    export POLYBAR_WLAN=
+    export POLYBAR_LAN=eno2
 elif [[ "$HNAME" == "ephi" ]]; then
-    polybar --reload ephi-left &
-    polybar --reload ephi-middle &
+    export POLYBAR_WLAN=wlp3s0
+    export POLYBAR_LAN=enp0s31f6
 elif [[ "$HNAME" == "chica" ]]; then
-    left_display="DP-4"
-    middle_display="DP-3-3"
-    right_display="eDP-1"
+    export POLYBAR_WLAN=wlp170s0
+    export POLYBAR_LAN=enp0s13f0u3u1
+fi
 
-    left_screen=""
-    middle_screen=""
-    right_screen=""
+export PRIMARY_MONITOR="$PRIMARY_DISPLAY"
+echo "PRIMARY_MONITOR=$PRIMARY_DISPLAY"
+polybar --reload primary -c "$DIR"/base/config.ini &
 
-    # See what's attached and launch bars accordingly
-    sa=($CONNECTED_SCREENS)
-    [[ ${sa[*]} =~ "$left_display" ]] && left_screen="true" || left_screen=""
-    [[ ${sa[*]} =~ "$right_display" ]] && right_screen="true" || right_screen=""
-
-    echo "Screens found: $sa"
-
-    if [[ $left_screen ]] &&  [[ $right_screen ]] ; then
-        echo "Loading multi monitor config..."
-        polybar --reload chica-left &
-        polybar --reload chica-middle &
-        polybar --reload chica-right &
-    else
-        echo "Loading single monitor config..."
-        polybar --reload onescreen &
-    fi
-else
-    echo "Using polybar config 'onescreen'"
-    polybar --reload onescreen &
+if [[ $SECONDARY_DISPLAYS ]]; then
+    for sec_mon in $SECONDARY_DISPLAYS ; do
+        export SECONDARY_MONITOR="$sec_mon"
+        echo "SECONDARY_MONITOR=$SECONDARY_MONITOR"
+        polybar --reload secondary -c "$DIR"/base/config.ini &
+    done
 fi
 
 
