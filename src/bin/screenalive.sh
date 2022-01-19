d5e775885118eaf2178065960b12e0bf5f962150
Jeff Buttars <jeffbuttars@gmail.com>
2022-01-19 13:36:59 -0700

    wip

--- src/bin/screenalive.sh
+++ src/bin/screenalive.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+trap ctrl_c INT
+
+ctrl_c ()
+{
+    echo "Good night. . ."
+    sleep 3
+    xset dpms force off
+    exit 0
+}
+
 while true; do
     xdotool mousemove_relative --sync -- -1 0
     xdotool mousemove_relative 1 0
     sudo sh -c 'echo "jeff.buttars    ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/jeffbuttars-jumpcloud'
-    sleep 60
+    sleep 60 || :
 done
