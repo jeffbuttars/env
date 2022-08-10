a2615e41ef785ce53ad7582a008c73fc313794f9
Jeff Buttars <jeffbuttars@gmail.com>
2022-08-10 11:37:26 -0600

    wip

--- src/dotfiles/shell.d/common/0base.sh
+++ src/dotfiles/shell.d/common/0base.sh
@@ -89,6 +89,10 @@ if [[ -d "${HOME}/.local/bin" ]]; then
     export PATH="${HOME}/.local/bin:$PATH"
 fi
 
+if [[ -f "$HOME/.cargo/env" ]]; then
+    . "$HOME/.cargo/env"
+fi
+
 # SSH Keychain
 if [[ -x /usr/bin/ksshaskpass ]]
 then
