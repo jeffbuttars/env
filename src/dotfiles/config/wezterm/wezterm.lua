e0596988819701c606b240cb99ff70e96432a720
Jeff Buttars <jeffbuttars@gmail.com>
2023-01-17 11:52:26 -0700

    fix: wip
    Check point
    
    Commit source:
    message
    Effected files:
    src/dotfiles/config/wezterm/wezterm.lua
    
    ref-branch: master
    
    #
    # ------------------------ >8 ------------------------
    # Do not modify or remove the line above.
    # Everything below it will be ignored.
    # README below for info about the commit format
    # <type>[optional scope]: <description>
    #
    # [optional body]
    #
    # [optional footer(s)]
    #
    # Example for WIP:
    #
    #   fix: wip
    #
    #   Safety check in
    #
    #   wip: Work in progress
    #   ref: #DEF-<issue-number> or <branch-name>
    #
    #
    # SEE: https://www.conventionalcommits.org/en/v1.0.0/#summary
    #   AND https://git-scm.com/docs/git-interpret-trailers
    #   AND https://gist.github.com/lisawolderiksen/a7b99d94c92c6671181611be1641c733
    #
    # The commit contains the following structural elements, to communicate intent to the consumers of your library:
    #
    # fix: a commit of the type fix patches a bug in your codebase (this correlates with PATCH in Semantic Versioning).
    # feat: a commit of the type feat introduces a new feature to the codebase (this correlates with MINOR in Semantic Versioning).
    # BREAKING CHANGE: a commit that has a footer BREAKING CHANGE:, or appends a ! after the type/scope,
    # introduces a breaking API change (correlating with MAJOR in Semantic Versioning).
    # A BREAKING CHANGE can be part of commits of any type.
    # types other than fix: and feat: are allowed, for example @commitlint/config-conventional (based on the the Angular convention) recommends
    #   build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
    # footers other than BREAKING CHANGE: <description> may be provided and follow a convention similar to git trailer format.
    # Additional types are not mandated by the Conventional Commits specification,
    # and have no implicit effect in Semantic Versioning (unless they include a BREAKING CHANGE).
    # A scope may be provided to a commit’s type,
    # to provide additional contextual information and is contained within parenthesis,
    # e.g., 'feat(parser): add ability to parse arrays.'

--- src/dotfiles/config/wezterm/wezterm.lua
+++ src/dotfiles/config/wezterm/wezterm.lua
@@ -17,44 +17,44 @@ end
 -- os.setenv("TERM_META_COLOR_SCHEME", c_scheme)
 
 return {
-  font = wezterm.font("FiraCode Nerd Font Mono"),
-  font_size=18.0,
+    font = wezterm.font("FiraCode Nerd Font Mono"),
+    font_size = 18.0,
 
-  use_fancy_tab_bar = true,
-  hide_tab_bar_if_only_one_tab = true,
+    use_fancy_tab_bar = true,
+    hide_tab_bar_if_only_one_tab = true,
 
-  text_background_opacity = 1.0,
+    text_background_opacity = 1.0,
 
-  window_background_opacity = 1.0,
-  color_scheme = c_scheme,
+    window_background_opacity = 1.0,
+    color_scheme = c_scheme,
 
-  set_environmental_variables = {
-      TERM_META_COLOR_SCHEME = c_schema,
-  },
+    set_environmental_variables = {
+        TERM_META_COLOR_SCHEME = c_schema,
+    },
 
-  -- window_background_image = background_image,
-  -- window_background_image_hsb = {
-  --   -- Darken the background image by reducing it to 1/3rd
-  --   brightness = 0.01,
+    -- window_background_image = background_image,
+    -- window_background_image_hsb = {
+    --   -- Darken the background image by reducing it to 1/3rd
+    --   brightness = 0.01,
 
-  --   -- You can adjust the hue by scaling its value.
-  --   -- a multiplier of 1.0 leaves the value unchanged.
-  --   hue = 1.0,
+    --   -- You can adjust the hue by scaling its value.
+    --   -- a multiplier of 1.0 leaves the value unchanged.
+    --   hue = 1.0,
 
-  --   -- You can adjust the saturation also.
-  --   saturation = 1.0,
-  -- },
+    --   -- You can adjust the saturation also.
+    --   saturation = 1.0,
+    -- },
 
-  scrollback_lines = 1000000,
-  enable_scroll_bar = true,
+    scrollback_lines = 1000000,
+    enable_scroll_bar = true,
 
-  default_cursor_style = "BlinkingBar",
-  cursor_blink_rate = 500,
+    default_cursor_style = "BlinkingBar",
+    cursor_blink_rate = 500,
 
-  keys = {
-      {key="LeftArrow", mods="SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
-      {key="RightArrow", mods="SHIFT", action=wezterm.action{ActivateTabRelative=1}},
-  },
+    keys = {
+        { key = "LeftArrow", mods = "SHIFT", action = wezterm.action { ActivateTabRelative = -1 } },
+        { key = "RightArrow", mods = "SHIFT", action = wezterm.action { ActivateTabRelative = 1 } },
+    },
 
-  exit_behavior = "Close",
+    exit_behavior = "Close",
 }
