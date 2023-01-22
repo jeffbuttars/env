d6b5150347fa45acf80e27c5e3afd563c9fcb0b6
Jeff Buttars <jeffbuttars@gmail.com>
2023-01-21 17:29:54 -0700

    fix: wip
    Check point
    
    Commit source:
    message
    Effected files:
    src/dotfiles/config/wezterm/wezterm.lua
    
    ref-branch: master
    
    #
    # ------------------------ >8 ------------------------
    # Do not modify or remove the line above.
    # Everything below it will be ignored.
    # README below for info about the commit format
    # <type>[optional scope]: <description>
    #
    # [optional body]
    #
    # [optional footer(s)]
    #
    # Example for WIP:
    #
    #   fix: wip
    #
    #   Safety check in
    #
    #   wip: Work in progress
    #   ref: #DEF-<issue-number> or <branch-name>
    #
    #
    # SEE: https://www.conventionalcommits.org/en/v1.0.0/#summary
    #   AND https://git-scm.com/docs/git-interpret-trailers
    #   AND https://gist.github.com/lisawolderiksen/a7b99d94c92c6671181611be1641c733
    #
    # The commit contains the following structural elements, to communicate intent to the consumers of your library:
    #
    # fix: a commit of the type fix patches a bug in your codebase (this correlates with PATCH in Semantic Versioning).
    # feat: a commit of the type feat introduces a new feature to the codebase (this correlates with MINOR in Semantic Versioning).
    # BREAKING CHANGE: a commit that has a footer BREAKING CHANGE:, or appends a ! after the type/scope,
    # introduces a breaking API change (correlating with MAJOR in Semantic Versioning).
    # A BREAKING CHANGE can be part of commits of any type.
    # types other than fix: and feat: are allowed, for example @commitlint/config-conventional (based on the the Angular convention) recommends
    #   build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
    # footers other than BREAKING CHANGE: <description> may be provided and follow a convention similar to git trailer format.
    # Additional types are not mandated by the Conventional Commits specification,
    # and have no implicit effect in Semantic Versioning (unless they include a BREAKING CHANGE).
    # A scope may be provided to a commit’s type,
    # to provide additional contextual information and is contained within parenthesis,
    # e.g., 'feat(parser): add ability to parse arrays.'

--- src/dotfiles/config/wezterm/wezterm.lua
+++ src/dotfiles/config/wezterm/wezterm.lua
@@ -1,26 +1,36 @@
 local wezterm = require 'wezterm';
 local TERM_META = os.getenv("TERM_META")
-local HOME = os.getenv("HOME")
+-- local HOME = os.getenv("HOME")
 
-local c_scheme = "Solarized (light) (terminal.sexy)"
+local rose_pine_dawn = require("lua/rose-pine-dawn")
+local rose_pine_moon = require("lua/rose-pine-moon")
+
+local colors = rose_pine_dawn.colors()
+local window_frame = rose_pine_dawn.window_frame()
+
+
+-- local c_scheme = "Solarized (light) (terminal.sexy)"
 -- local c_scheme = "Spring"
 -- local c_scheme = "dayfox"
 -- local c_scheme = "dawnfox"
 
-local background_image = HOME .. "/Pictures/wallpaper/spikegungs.jpg"
+-- local background_image = HOME .. "/Pictures/wallpaper/spikegungs.jpg"
 
 if (TERM_META == "dark")
 then
     -- c_scheme = "Solarized Dark - Patched"
     -- c_scheme = "nordfox"
-    c_scheme = "terafox"
-    background_image = HOME .. "/Pictures/wallpaper/spikegungs.jpg"
+    -- c_scheme = "terafox"
+    -- c_scheme = "rose-pine-moon"
+    -- background_image = HOME .. "/Pictures/wallpaper/spikegungs.jpg"
+    colors = rose_pine_moon.colors()
+    window_frame = rose_pine_moon.window_frame()
 end
 
-if os.getenv("TERM_META_COLOR_SCHEME")
-then
-    c_scheme = os.getenv("TERM_META_COLOR_SCHEME")
-end
+-- if os.getenv("TERM_META_COLOR_SCHEME")
+-- then
+--     c_scheme = os.getenv("TERM_META_COLOR_SCHEME")
+-- end
 
 -- The art is a bit too bright and colorful to be useful as a backdrop
 -- for text, so we're going to dim it down to 10% of its normal brightness
@@ -36,7 +46,10 @@ return {
     text_background_opacity = 1.0,
 
     window_background_opacity = 1.0,
-    color_scheme = c_scheme,
+    -- color_scheme = c_scheme,
+
+    colors = colors,
+    window_frame = window_frame,
 
     -- This fixes font/terminal resizing issues when using a tiling WM
     adjust_window_size_when_changing_font_size = false,
