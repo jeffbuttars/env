379a022884cbbfcc1ae70f5391e3a75474457099
Jeff Buttars <jeffbuttars@gmail.com>
2023-02-15 15:08:51 -0700

    fix:
    Commit source:
    message
    Effected files:
    src/bin/setup_bst_ubuntu.sh
    src/dotfiles/config/yakuakerc
    src/dotfiles/local/share/konsole/red.profile
    src/dotfiles/shell.d/common/70nvm.sh
    src/dotfiles/zshrc
    src/poetry.lock
    
    ref-branch: master
    
    #
    # ------------------------ >8 ------------------------
    # Do not modify or remove the line above.
    # Everything below it will be ignored.
    # README below for info about the commit format
    # <type>[optional scope]: <description>
    #
    # [optional body]
    #
    # [optional footer(s)]
    #
    # Example for WIP:
    #
    #   fix: wip
    #
    #   Safety check in
    #
    #   wip: Work in progress
    #   ref: #DEF-<issue-number> or <branch-name>
    #
    #
    # SEE: https://www.conventionalcommits.org/en/v1.0.0/#summary
    #   AND https://git-scm.com/docs/git-interpret-trailers
    #   AND https://gist.github.com/lisawolderiksen/a7b99d94c92c6671181611be1641c733
    #
    # The commit contains the following structural elements, to communicate intent to the consumers of your library:
    #
    # fix: a commit of the type fix patches a bug in your codebase (this correlates with PATCH in Semantic Versioning).
    # feat: a commit of the type feat introduces a new feature to the codebase (this correlates with MINOR in Semantic Versioning).
    # BREAKING CHANGE: a commit that has a footer BREAKING CHANGE:, or appends a ! after the type/scope,
    # introduces a breaking API change (correlating with MAJOR in Semantic Versioning).
    # A BREAKING CHANGE can be part of commits of any type.
    # types other than fix: and feat: are allowed, for example @commitlint/config-conventional (based on the the Angular convention) recommends
    #   build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
    # footers other than BREAKING CHANGE: <description> may be provided and follow a convention similar to git trailer format.
    # Additional types are not mandated by the Conventional Commits specification,
    # and have no implicit effect in Semantic Versioning (unless they include a BREAKING CHANGE).
    # A scope may be provided to a commit’s type,
    # to provide additional contextual information and is contained within parenthesis,
    # e.g., 'feat(parser): add ability to parse arrays.'

--- src/dotfiles/shell.d/common/70nvm.sh
+++ src/dotfiles/shell.d/common/70nvm.sh
@@ -8,40 +8,46 @@
 # echo "nvm not installed?"
 # echo 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash'
 
-install_nvm()
-{
+install_nvm() {
     local nvm_ver='v0.37.2'
     echo "installing nvm $nvm_ver ..."
     echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$nvm_ver/install.sh | bash"
     curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${nvm_ver}/install.sh" | bash
 }
 
-# mkdir -p $HOME/.nvm
-# export NVM_DIR=$HOME/.nvm
-# [ -f $NVM_DIR/nvm.sh ] && source $NVM_DIR/nvm.sh
+load_nvm() {
+    # mkdir -p $HOME/.nvm
+    # export NVM_DIR=$HOME/.nvm
+    # [ -f $NVM_DIR/nvm.sh ] && source $NVM_DIR/nvm.sh
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use # This loads nvm
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use # This loads nvm
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    # export NVM_DIR="$HOME/.nvm"
+    # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+    if [[ -z $NVM_DIR ]]; then
+        # no nvm
+        # Localize the global nodejs installs
+        export NPM_PACKAGES="$HOME/.npm-packages"
 
-if [[ -z $NVM_DIR  ]]; then
-    # no nvm
-    # Localize the global nodejs installs
-    export NPM_PACKAGES="$HOME/.npm-packages"
+        export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 
-    export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+        if [[ ! -d "$NPM_PACKAGES" ]]; then
+            mkdir -p "$NPM_PACKAGES"
+        fi
 
-    if [[ ! -d "$NPM_PACKAGES" ]]; then
-        mkdir -p "$NPM_PACKAGES"
+        # MANPATH
+        export MANPATH="$(manpath):$NPM_PACKAGES/share/man"
     fi
 
-    # MANPATH
-    export MANPATH="$(manpath):$NPM_PACKAGES/share/man"
-fi
+    # Use the default env
+    nvm_ver_to_use='node'
 
-# Use the default env
-nvm use default
+    if [[ -n "$1" ]]; then
+        nvm_ver_to_use="$1"
+    fi
+
+    nvm use "$nvm_ver_to_use"
+}
